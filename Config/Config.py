d3c1fd603394038654fc2650face58a4fd64ea06
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-11 20:06:33 +0530

    Update Config.py

--- Config/Config.py
+++ Config/Config.py
@@ -2,7 +2,7 @@ import os
 
 OWNER_ID = int(os.environ.get("OWNER_ID", 12345))  # Replace with your Telegram ID
 SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "12345 67890").split()))
-API_ID = int(os.environ.get("TELEGRAM_API_ID", "YOUR_API_ID"))
+API_ID = int(os.environ.get("TELEGRAM_API_ID", YOUR_API_ID))
 API_HASH = os.environ.get("TELEGRAM_API_HASH", "YOUR_API_HASH")
 BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "YOUR_BOT_TOKEN")
 MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "YOUR_MONGO_DB_URI") # If using MongoDB
