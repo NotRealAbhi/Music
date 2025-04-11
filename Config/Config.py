37833f68c4b94ac82fc8cada25b6babf9cf0641b
Abhishek Thakur <alaricabhi@gmail.com>
2025-04-11 20:05:50 +0530

    Update Config.py

--- Config/Config.py
+++ Config/Config.py
@@ -5,4 +5,4 @@ SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "12345 67890").split()))
 API_ID = int(os.environ.get("TELEGRAM_API_ID", "YOUR_API_ID"))
 API_HASH = os.environ.get("TELEGRAM_API_HASH", "YOUR_API_HASH")
 BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN", "YOUR_BOT_TOKEN")
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "YOUR_MONGO_DB_URI")) # If using MongoDB
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "YOUR_MONGO_DB_URI") # If using MongoDB
